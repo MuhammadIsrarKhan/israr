17c59b2c99c623d427ce78a942918364b647ef11
israruetp <israruetp@gmail.com>
2024-07-06 17:14:27 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+console.log(`
+
+Muhammad Israr Khan
+
+Versatile Software Engineer, crafting challenging products
+with extra attention to detail. 2+ years of experience.
+
+Email - israruetp@gmail.com
+Github - https://github.com/MuhammadIsrarKhan
+LinkedIn - https://www.linkedin.com/in/muhammad-israr-khan-558300199/
+
+`);
