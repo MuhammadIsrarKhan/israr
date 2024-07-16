2dbab24e59e13d36d527939c214cae6021211fe0
MuhammadIsrarKhan <israruetp@gmail.com>
2024-07-16 08:14:09 +0000

    📦 NEW: improve help portion of cli

--- utils/cli.js
+++ utils/cli.js
@@ -1,43 +1,47 @@
 const meow = require('meow');
-const {green,yellow,cyan} = require('chalk');
+const meowHelp = require('cli-meow-help');
 
-
-const helpText = `
-Usage
-${green('npx israri')} ${yellow('[--option]')} ${cyan('<command>')}
-Options
-${yellow('--social')}       Show the social info
-${yellow('--no-social')}    Don't show social info
-${yellow('-d')}, ${yellow('--debug')}    Print debug informations
-${yellow('-m')}, ${yellow('--minimal')}  Print minimal informations
-${yellow('-v')}, ${yellow('--version')}  Print CLI version
-
-Commands
-${cyan('<help>')}         Print CLI help information
-
-Examples
-${green('npx israri')} --no-social
-`
-const options = {
-    flags : {
-        minimal : {
-            type : 'boolean',
-            alias : 'm'
-        },
-        social : {
-            type : 'boolean',
-            default : true
-        },
-        debug : {
-            type : 'boolean',
-            default : false,
-            alias : 'd'
-        },
-        version : {
-            type : 'boolean',
-            default : false,
-            alias : 'v'
-        },
-    }
+const flags = {
+    minimal : {
+        type : 'boolean',
+        alias : 'm',
+        desc : 'Print minimal information'
+    },
+    social : {
+        type : 'boolean',
+        default : true,
+        desc : 'Show the social info'
+    },
+    debug : {
+        type : 'boolean',
+        default : false,
+        alias : 'd',
+        desc : 'Print debug informations'
+    },
+    version : {
+        type : 'boolean',
+        default : false,
+        alias : 'v',
+        desc : 'Print CLI version'
+    },
 }
-module.exports = meow(helpText,options);
+
+const commands = {
+	help: {
+		desc: `Print the help info`,
+	},
+};
+
+const helpText = meowHelp({
+	name: `npx awais`,
+	flags,
+	commands,
+});
+
+const options = {
+	inferType: true,
+	hardRejection: false,
+	flags,
+};
+
+module.exports = meow(helpText, options);
